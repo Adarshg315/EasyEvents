b46d26a80aba2533e1abe4220ce6a0fe90224f38
Adarshg315 <adarshg315@gmail.com>
2021-02-15 13:01:03 +0530

    connected user and events

---
+++ models/event.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const eventSchema = new Schema({
+	title: {
+		type: String,
+		required: true,
+	},
+	description: {
+		type: String,
+		required: true,
+	},
+	date: {
+		type: Date,
+		// required: true,
+	},
+	creator: {
+		type: Schema.Types.ObjectId,
+		ref: "User",
+	},
+});
+
+module.exports = mongoose.model("Event", eventSchema);
