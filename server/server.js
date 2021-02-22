21d1d85a60b3a27739d3aa8f91d3f5cb812c775f
Adarshg315 <adarshg315@gmail.com>
2021-02-22 17:27:15 +0530

    configuring server

--- server/server.js
+++ server/server.js
@@ -35,6 +35,13 @@ const PORT = process.env.port || 5000;
 const connectDB = require("./config/db.config");
 connectDB();
 
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static("client/build/"));
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "client", "build", "index.hmtl"));
+	});
+}
+
 app.get("/", (req, res) => {
 	res.send("Hello from Express!");
 });
