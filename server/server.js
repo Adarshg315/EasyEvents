ea9041d7ff4635a19ae484509d996a235f1afc0f
Adarshg315 <adarshg315@gmail.com>
2021-02-22 17:04:57 +0530

    jgnn

--- server/server.js
+++ server/server.js
@@ -35,16 +35,10 @@ const PORT = process.env.port || 5000;
 const connectDB = require("./config/db.config");
 connectDB();
 
-// app.get("*", (req, res) => {
-// 	res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-// });
+app.get("/", (req, res) => {
+	res.send("Hello from Express!");
+});
 
-app.get('/', (req, res) => { res.send('Hello from Express!')
-
-
-// if (process.env.NODE_ENV === "production") {
-// 	app.use(express.static("client/build"));
-// }
 app.listen(PORT, () => {
 	console.log(`Server started at PORT ${PORT}`);
 });
